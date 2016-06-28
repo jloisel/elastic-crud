fd8bd260ef1df7be3ad460b362e49fbd702a248a
jloisel <loisel.jerome@gmail.com>
2016-06-28 09:19:18 +0200

    junit

---
+++ db-integration-test/src/test/java/com/jeromeloisel/db/integration/test/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.jeromeloisel.db.integration.test;
+
+import com.jeromeloisel.Application;
+import org.junit.Test;
+
+public class ApplicationTest {
+
+  @Test
+  public void shouldRunMain() throws Exception {
+    Application.main(new String[0]);
+  }
+}
